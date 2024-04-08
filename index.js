85fd215d266c6d27391acb0b3084f314e32f158c
Rajeevverma20 <rv208469@gmail.com>
2024-04-08 15:08:15 +0530

    done

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const userRoutes= require('./routes/userRoutes');
 const courseRoutes = require('./routes/courseRoutes');
 const enrollmentRoutes = require('./routes/enrollmentRoutes');
+const morgan = require('morgan');
 const app = express();
 const port = 8000;
 
 
 app.use(express.json());
-
+app.use(morgan("tiny"));
 
 app.use('/learning', userRoutes, courseRoutes, enrollmentRoutes);
 
